d89b4e4f34eabd47cd853b8737fa5c299bc4e46a
yoshiodei <yoshiodei@yahoo.com>
2022-01-14 02:20:38 +0000

    create nav components

---
+++ src/components/SideNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidenav = () => {
+    return (
+        <nav className='side-nav'>
+            
+        </nav>
+    );
+}
+
+export default Sidenav;
---
+++ src/components/TopNav.jsx
@@ -0,0 +1,16 @@
+import "./../styles/top-nav";
+
+const Topnav = () => {
+    return (
+        <nav className='top-nav'>
+            <div className="top-nav__profile-div">
+
+            </div>
+            <button className="top-nav__logout-btn">
+                Log Out
+            </button>
+        </nav>
+    );
+}
+
+export default Topnav;
