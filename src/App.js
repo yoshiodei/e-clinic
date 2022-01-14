747b0b28a87d6f6a8c0e6132aa2f443a1740b5a2
yoshiodei <yoshiodei@yahoo.com>
2022-01-14 03:53:44 +0000

    remove used comments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Mainsection from "./components/MainSection";
+import Sidenav from "./components/SideNav";
+
 
 function App() {
   return (
     <div className="App">
-      
+        <Sidenav />
+        <Mainsection />
     </div>
   );
 }
