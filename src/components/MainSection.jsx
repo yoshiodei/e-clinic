9ee80ad04cda5274a1620cf0865ccd4f1259f321
yoshiodei <yoshiodei@yahoo.com>
2022-01-14 02:50:58 +0000

    add MainSection component

---
+++ src/components/MainSection.jsx
@@ -0,0 +1,12 @@
+import Topnav from './TopNav';
+import "./../styles/main-section.css";
+
+const Mainsection = () => {
+    return (
+        <main className='main-section'>
+            <Topnav />
+        </main>
+    );
+}
+
+export default Mainsection;
