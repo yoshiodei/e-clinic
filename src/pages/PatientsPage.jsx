5aa9be8374c7fca8f072a02155c6953b5bd13f13
Bill Zack <bzak954@gmail.com>
2022-01-16 20:40:22 +0000

    render bw component

--- src/pages/PatientsPage.jsx
+++ src/pages/PatientsPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Patientsregistration from './Patient form/PatientsRegistration';
 import Patients from './Patients/Patients';
 
 const Patientspage = () => {
     return (
         <section className='main-page-section'>
-            <Patients/>
+            {/* <Patients/> */}
+            <Patientsregistration/>
         </section>
     );
 }
