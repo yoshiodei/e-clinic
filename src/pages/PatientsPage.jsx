b6ff8286e79f6b1eefaea0803a68781b677ac1e7
Bill Zack <bzak954@gmail.com>
2022-01-17 13:30:58 +0000

    add new component

--- src/pages/PatientsPage.jsx
+++ src/pages/PatientsPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import Accounts from './Accounts Page/Accounts';
 import Patientsregistration from './Patient form/PatientsRegistration';
+import Patientinfomodal from './Patient Information Modal/PatientInfoModal';
 import Patients from './Patients/Patients';
 
 const Patientspage = () => {
     return (
         <section className='main-page-section'>
             {/* <Patients/> */}
-            <Patientsregistration/>
+            {/* <Patientsregistration/> */}
+            {/* <Accounts/> */}
+            <Patientinfomodal/>
         </section>
     );
 }
