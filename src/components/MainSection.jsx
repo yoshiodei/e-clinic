a0d9322cd93e4a4a3a214cdbb8dd4d28464019f9
Bill Zack <bzak954@gmail.com>
2022-01-21 16:56:45 +0000

    update props

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -3,7 +3,7 @@ import "./../styles/main-section.css";
 import Patientinfo from './PatientInfo';
 import Pagecontent from './PageContent';
 
-const Mainsection = ({props}) => {
+const Mainsection = (props) => {
     return (
         <main className='main-section'>
             <Topnav />
