7121554efbee905caa400bcb567fee2a9a290736
Bill Zack <bzak954@gmail.com>
2022-01-25 14:01:10 +0000

    pass patients props

--- src/components/PageContent.jsx
+++ src/components/PageContent.jsx
@@ -3,14 +3,17 @@ import Patientspage from "../pages/PatientsPage";
 import Testpage from '../pages/TestPage';
 import "./../styles/page-content.css";
 
-const Pagecontent = (props) => {
+const Pagecontent = ({patients}) => {
+
+    
+
     return (
         <div className="page-content">
             {/* the code for the page component goes here */}
             {/* give it a class name of "main-page-component" to apply default styling */}
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Patientspage props={props}/>}/> 
+                    <Route path="/" element={<Patientspage patients={patients}/>}/> 
                     <Route path="/test" element={<Testpage/>}/> 
                 </Routes>
             </BrowserRouter>
