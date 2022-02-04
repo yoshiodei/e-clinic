9aa0cf1031a0f13f9ddde9e168c128ca4ac2ff7c
princeantwi2k <antwi2k@gmail.com>
2022-02-04 11:08:41 +0800

    added MainSection

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,16 +1,16 @@
-import Topnav from './TopNav';
+import Topnav from "./TopNav";
 import "./../styles/main-section.css";
-import Patientinfo from './PatientInfo';
-import Pagecontent from './PageContent';
+import Patientinfo from "./PatientInfo";
+import Pagecontent from "./PageContent";
 
-const Mainsection = ({patients}) => {
-    return (
-        <main className='main-section'>
-            <Topnav />
-            <Patientinfo   />
-            <Pagecontent patients={patients} />
-        </main>
-    );
-}
+const Mainsection = ({ patients }) => {
+  return (
+    <main className="main-section">
+      <Topnav />
+      <Patientinfo />
+      <Pagecontent patients={patients} />
+    </main>
+  );
+};
 
 export default Mainsection;
