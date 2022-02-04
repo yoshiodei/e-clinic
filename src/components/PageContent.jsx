b0be60039ec000e04d0fd6ce746ed77e3b88402c
princeantwi2k <antwi2k@gmail.com>
2022-02-04 11:09:17 +0800

    added pageContent

--- src/components/PageContent.jsx
+++ src/components/PageContent.jsx
@@ -1,22 +1,26 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Consultation from '../pages/Consultation';
-import Patientspage from "../pages/PatientsPage";
-import Testpage from '../pages/TestPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Adddrugs from "../pages/AddDrugs";
+import Consultation from "../pages/Consultation";
+
+import Pharmacy from "../pages/Pharmacy";
+import Testpage from "../pages/TestPage";
 import "./../styles/page-content.css";
 
-const Pagecontent = ({patients}) => {
-    return (
-        <div className="page-content">
-            {/* the code for the page component goes here */}
-            {/* give it a class name of "main-page-component" to apply default styling */}
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Consultation  patients={patients}/>}/> 
-                    <Route path="/test" element={<Testpage/>}/> 
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
-}
+const Pagecontent = ({ patients }) => {
+  return (
+    <div className="page-content">
+      {/* the code for the page component goes here */}
+      {/* give it a class name of "main-page-component" to apply default styling */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Consultation patients={patients} />} />
+          <Route path="/test" element={<Testpage />} />s
+          <Route path="/pharmacy" element={<Pharmacy patients={patients} />} />
+          <Route path="/add" element={<Adddrugs />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default Pagecontent;
