6cdbcf73e6f50db0f0fa56c9a8a287f80a642255
princeantwi2k <antwi2k@gmail.com>
2022-02-04 11:10:15 +0800

    added sideNav

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,34 +1,35 @@
 import "./../styles/side-nav.css";
 
 const Sidenav = () => {
-    return (
-        <nav className='side-nav'>
-            <div className="title-div">
-                <h1>E-Clinic</h1>
-            </div>
-            <div className="item-div active">
-                <h5>Patients</h5>
-            </div>    
-            <div className="item-div">
-                <h5>Consultation</h5>
-            </div>    
-            <div className="item-div">
-                <h5>Examination</h5>
-            </div>    
-            <div className="item-div">
-                <h5>Accounts</h5>
-            </div>    
-            <div className="item-div">
-                <h5>Pharmacy</h5>
-            </div>    
-            <div className="item-div">
-                <h5>Records</h5>
-            </div>    
-            <div className="item-div">
-                <h5>Set up</h5>
-            </div>    
-        </nav>
-    );
-}
+  return (
+    <nav className="side-nav">
+      <div className="title-div">
+        <h1>E-Clinic</h1>
+      </div>
+      <div className="item-div ">
+        <h5>Patients</h5>
+      </div>
+      <div className="item-div">
+        <h5>Consultation</h5>
+      </div>
+      <div className="item-div">
+        <h5>Examination</h5>
+      </div>
+
+      <div className="item-div">
+        <h5>Accounts</h5>
+      </div>
+      <div className="item-div">
+        <h5>Pharmacy</h5>
+      </div>
+      <div className="item-div">
+        <h5>Records</h5>
+      </div>
+      <div className="item-div">
+        <h5>Set up</h5>
+      </div>
+    </nav>
+  );
+};
 
 export default Sidenav;
